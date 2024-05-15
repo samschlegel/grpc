367764f89a94d85895e2edd2923f39a28ac9af7f
Sam Schlegel <sam.s@discordapp.com>
2024-05-15 22:12:26 +0000

    idk

--- setup.py
+++ setup.py
@@ -503,6 +503,7 @@ except ImportError:
             "You requested a Cython build via GRPC_PYTHON_BUILD_WITH_CYTHON, "
             "but do not have Cython installed. We won't stop you from using "
             "other commands, but the extension files will fail to build.\n")
+        SETUP_REQUIRES += ("cython>=0.23,<3",)
     elif need_cython:
         sys.stderr.write(
             "We could not find Cython. Setup may take 10-20 minutes.\n")
